4704c71a060d960379a01843caa4f0e2b96725c2
GostyuninVA <48084264+GostyuninVA@users.noreply.github.com>
2021-04-23 17:45:17 +0300

    Initial commit

---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerInput : MonoBehaviour
+{
+    private Vector3 _tapPosition;
+
+    public event UnityAction<Vector2> DirectionChange;
+    public event UnityAction PointerUp;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _tapPosition = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            DirectionChange?.Invoke(Input.mousePosition - _tapPosition);
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            PointerUp?.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/Player/States/MoveState.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveState : PlayerState
+{
+    [SerializeField] private StaminaAccumulator _staminaAccumulator;
+
+    [SerializeField] private PlayerInput _playerInput;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _speedRatio;
+
+    private void OnEnable()
+    {
+        _playerInput.DirectionChange += OnDirectionChanged;
+        _staminaAccumulator.StartAccumulate();
+    }
+
+    private void OnDisable()
+    {
+        _playerInput.DirectionChange -= OnDirectionChanged;
+        Animator.SetFloat("run", 0);
+    }
+
+    private void OnDirectionChanged(Vector2 direction)
+    {
+        Rigidbody.velocity = new Vector3(direction.x, 0, direction.y) * _speedRatio;
+
+        if(Rigidbody.velocity.magnitude > _speed)
+        {
+            Rigidbody.velocity *= _speed / Rigidbody.velocity.magnitude;
+        }
+
+        if(Rigidbody.velocity.magnitude != 0)
+        {
+            Rigidbody.MoveRotation(Quaternion.LookRotation(Rigidbody.velocity, Vector3.up));
+        }
+    }
+
+    private void Update()
+    {
+        Animator.SetFloat("run", Rigidbody.velocity.magnitude);
+        Debug.Log(Rigidbody.velocity.magnitude);
+    }
+}
